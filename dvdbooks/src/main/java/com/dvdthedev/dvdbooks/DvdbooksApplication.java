3ca2fe9cce635ad901c37bc8b2105d33c0fc05df
Deivid Rocha <deividzor@gmail.com>
2024-06-20 11:29:30 -0300

    fist commit

---
+++ dvdbooks/src/main/java/com/dvdthedev/dvdbooks/DvdbooksApplication.java
@@ -0,0 +1,13 @@
+package com.dvdthedev.dvdbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvdbooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvdbooksApplication.class, args);
+	}
+
+}
---
+++ dvdbooks/src/test/java/com/dvdthedev/dvdbooks/DvdbooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dvdthedev.dvdbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvdbooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
